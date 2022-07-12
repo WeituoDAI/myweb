953342c52c3a13a9a8b2fd232b93b13b0cd7beb1
weituo <weituo.dai@polytechnique.edu>
2022-07-12 16:01:46 +0200

    add a dataset house.csv(regression problem)

--- myscript.py
+++ myscript.py
@@ -7,10 +7,9 @@ from skrules import SkopeRules
 from sklearn.manifold import TSNE
 import umap
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
-
 
 import xgboost as xgb
 
@@ -27,8 +26,8 @@ def mytsne(X):
 	tsne=TSNE(n_components=2,init='random',learning_rate=200,random_state=0)
 	return tsne.fit_transform(X)
 
-def Dataset():
-	return pd.read_csv('static/BN_XGB_SHAP.csv', encoding='latin') 
+def Dataset(filename):
+	return pd.read_csv(filename, encoding='latin') 
 
 # get the feature names
 def getvariable(data):
@@ -36,15 +35,14 @@ def getvariable(data):
 	b= len(a)
 	return a[:b//2],a[b//2:],data.columns[-1]
 
-
-def Myfun0(fun,pathtosave):
-	data = Dataset()
+def Myfun0(fun,pathtosave,filename):
+	data = Dataset(filename)
 	(variable, shapvariable, target) = getvariable(data)
 	X=data[variable]
 	X_shap = data[shapvariable]
 	y = data[target]
 	#the initial data values needs the standarization(but the shap values does not)
-	newX=fun(StandardScaler().fit_transform(X))
+	newX=fun(MinMaxScaler().fit_transform(X))
 	newX_shap = fun(X_shap)
 	#save the file with data after pca()/umap()/tsne()
 	newdata = pd.DataFrame()
@@ -55,14 +53,14 @@ def Myfun0(fun,pathtosave):
 	newdata['label']=y
 	newdata.to_csv(pathtosave,header=True,index=None)
 	
-def Myfun_tsne():
-	Myfun0(mytsne,"static/data_tsne.csv")
+def Myfun_tsne(filename_tsne,filename):
+	Myfun0(mytsne,filename_tsne,filename)
 
-def Myfun_pca():
-	Myfun0(mypca,"static/data_pca.csv")
+def Myfun_pca(filename_pca,filename):
+	Myfun0(mypca,filename_pca,filename)
 
-def Myfun_umap():
-	Myfun0(myumap,"static/data_umap.csv")
+def Myfun_umap(filename_umap,filename):
+	Myfun0(myumap,filename_umap,filename)
 		
 def Myskope(X,y,Feature_names):
 	skp = SkopeRules(
@@ -84,13 +82,14 @@ def Myskope(X,y,Feature_names):
 	
 	return {"rule":myrule,"precision":precision,"recall":recall},prediction
 
-def Myfun_skope(a):
-	dataset=pd.read_csv('static/banknote.csv', encoding='latin') 		
+def Myfun_skope(a,filename,original_filename):
+	
+	dataset=Dataset(original_filename) 		
 	X=dataset.drop(axis=1, labels=dataset.columns[-1],inplace=False)		
 	Feature_names=dataset.columns[:-1]
 	(rules,prediction)=Myskope(X,a,Feature_names)
 	
-	Label = Dataset()['label']#this is the prediction of xgboost
+	Label = Dataset(filename)['label']#this is the prediction of xgboost
 	
 	p=0#average prediction(xgboost) of points selected
 	q=0#average prediction(xgboost) of points triggered by the rule
@@ -112,7 +111,7 @@ def mykmeans(X,N_clusters):
 	return list(kmeans.fit_predict(X))
 		
 def Mykmeans_load(filename,N_clusters):
-	data = pd.read_csv(filename,encoding='latin')
+	data = Dataset(filename)
 	X = data[['shap0','shap1']]
 	z= mykmeans(X,N_clusters)
 	return z
@@ -126,14 +125,14 @@ def mydbscan(epsilon,M,X):
 	return list(z)
 
 def Mydbscan_load(filename,epsilon,M):
-	data = pd.read_csv(filename,encoding='latin')
+	data = Dataset(filename)
 	X = data[['shap0','shap1']]
 	z= mydbscan(epsilon,M,X)
 	N=len(np.unique(z))
 	return z,N
 	
 
-def myxgb(X,y):
+def myxgbclf(X,y):
 	clf = xgb.XGBClassifier(
 		n_estimators=50,
 		enable_categorical=False,  #  si True, tree_method = "gpu_hist" or "hist" or "approx"
@@ -144,16 +143,27 @@ def myxgb(X,y):
 	clf.fit(X,y)
 	return clf.feature_importances_
 
+def myxgbrg(X,y):
+	rg = xgb.XGBRegressor(
+		n_estimators=50,
+		enable_categorical=False,  #  si True, tree_method = "gpu_hist" or "hist" or "approx"
+		tree_method="approx",      # "gpu_hist" or "hist" or "approx" or "exact"
+		random_state=0
+		#max_depth,booster,tree_method,learning_rate ...
+	)
+	rg.fit(X,y)
+	return rg.feature_importances_
 
-def Myfun_xgb1(a,J):
-	data = Dataset()
+# a is an 0-1 array of size = data.length. it corresponds a subdataset (1-0 means inclus or not)
+def Myfun_xgb(a,J,filename,original_filename,pb):
+	data = Dataset(filename)
 	(variable, shapvariable, target) = getvariable(data)
 	X=data[variable]
 	
 	if(J=='1'or J=='3'):
 		y = data[target]
 	elif(J =='2'or J=='4'):
-		dataset =pd.read_csv('static/banknote.csv', encoding='latin') 		
+		dataset =Dataset(original_filename)		
 		y = dataset[dataset.columns[-1]]
 
 	X_new = []
@@ -173,8 +183,10 @@ def Myfun_xgb1(a,J):
 	for i in range(m):
 		X_NEW[variable[i]]=X_new[i]
 	
-			
-	fti = myxgb(X_NEW,y_new)
+	if(pb=='classification'):	
+		fti = myxgbclf(X_NEW,y_new)
+	else:
+		fti = myxgbrg(X_NEW,y_new)
 	z = {}
 	for i in range(m):
 		z[variable[i]]=fti[i]
@@ -187,8 +199,11 @@ def Myfun_xgb1(a,J):
 	
 
 
-	
-	
+def problemtype(data):
+	if data =='banknote':
+		return 'classification'
+	elif data =='house':
+		return 'regression'
 	
 	
 
--- myweb.py
+++ myweb.py
@@ -3,55 +3,84 @@ from flask_cors import CORS
 from flask import Flask,render_template, request, session
 from myscript import *
 
-#import os
-#from werkzeug.utils import secure_filename
-#UPLOAD_FOLDER = ""
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 CORS(app)
 
 app.secret_key = "bonjour"
 
-## transform string a to an array
+
 def Getlabel(a):
 	b = a.split(",")
 	for i in range(len(b)):
 		b[i]=int(b[i])
 	return b	
 	
+	
+	
 @app.route('/',methods=['POST','GET'])
 def index():
 	return render_template("index.html")
 
+###########################################################
+@app.route('/banknote',methods=['POST','GET'])
+def banknote():
+	if request.method == 'POST':
+		session['data']='banknote'
+		session['original_file']='static/'+session['data']+'/'+session['data']+'.csv'
+		session['XGB_SHAP_file']='static/'+session['data']+'/XGB_SHAP.csv'
+		session['pb']=problemtype(session['data'])
+		return render_template(session['data']+".html")
+	return render_template('index.html')
+
+@app.route('/house',methods=['POST','GET'])
+def house():
+	if request.method == 'POST':
+		session['data']='house'
+		session['original_file']='static/'+session['data']+'/'+session['data']+'.csv'
+		session['XGB_SHAP_file']='static/'+session['data']+'/XGB_SHAP.csv'
+		session['pb']=problemtype(session['data'])
+		return render_template(session['data']+".html")
+	return render_template('index.html')
+
+#############################################################
 @app.route('/tsne',methods=['POST','GET'])
 def tsne():	
 	if(request.method=="POST"):
-		Myfun_tsne()	
-		session['filename']="static/data_tsne.csv"
+		filename = session['XGB_SHAP_file']
+		pathtosave = "static/"+session['data']+"/data_tsne.csv"
+		#Myfun_tsne(pathtosave,filename)
+		session['filename']=pathtosave
 		session['rd']='tsne'
-		return render_template("cluster_skope-rules.html", filename =session['filename'], rd=session['rd'])
-	return render_template("index.html")
+		return render_template("cluster_skope-rules.html", filename =session['filename'], rd=session['rd'],pb= session['pb'])
+	return render_template(session['data']+".html")
 
 @app.route('/pca',methods=['POST','GET'])
 def pca():
 	if(request.method=="POST"):
-		Myfun_pca()
+		filename = session['XGB_SHAP_file']
+		pathtosave = "static/"+session['data']+"/data_pca.csv"
+		#Myfun_pca(pathtosave,filename)
+		session['filename']=pathtosave
 		session['rd']='pca'
-		session['filename']="static/data_pca.csv"
-		return render_template("cluster_skope-rules.html", filename =session['filename'] ,rd=session['rd'])
-	return render_template("index.html")
+		return render_template("cluster_skope-rules.html", filename =session['filename'] ,rd=session['rd'],pb= session['pb'])
+	return render_template(session['data']+".html")
 
 @app.route('/umap',methods=['POST','GET'])
 def umap():	
 	if(request.method=="POST"):
-		Myfun_umap()
+		filename = session['XGB_SHAP_file']
+		pathtosave = "static/"+session['data']+"/data_umap.csv"
+		#Myfun_umap(pathtosave,filename)
+		session['filename']=pathtosave
 		session['rd']='umap'
-		session['filename']="static/data_umap.csv"
-		return render_template("cluster_skope-rules.html", filename =session['filename'],rd=session['rd'])
-	return render_template("index.html")
+		return render_template("cluster_skope-rules.html", filename =session['filename'],rd=session['rd'],pb= session['pb'])
+	return render_template(session['data']+".html")
+##################################################
 
+#skope-rules
 @app.route('/run',methods=['POST','GET'])
 def run():
 	b=Getlabel(request.form.get("ids"))
@@ -59,16 +88,19 @@ def run():
 		return '0' ##so that the front-end gives an alert
 	else:
 		session['number']=sum(b)
-		x = Myfun_skope(b)
+		x = Myfun_skope(b,session['filename'],session['original_file'])
 		session['pf']=str(x[5])[1:-1]
 	return {'rule':x[0],'nop':x[1],'avp':x[2],'pfr':x[3],'avpr':x[4],'pf':session['pf']}
+#
 	
+
 @app.route('/kmeans',methods=['POST'])
 def kmeans():
 	a = request.form.get("n")
 	N_clusters=int(a)
 	z = Mykmeans_load(session['filename'],N_clusters)
 	return str(z)
+#	return the result of kmeans
 
 @app.route('/dbscan',methods=['POST'])
 def DBSCAN():
@@ -78,7 +110,9 @@ def DBSCAN():
 	M= int(M)	
 	(z,N) = Mydbscan_load(session['filename'],epsilon,M)
 	return {"label":str(z),"numberofcluster":str(N)}
+# 	return result of dbscan and number of cluster
 
+# do the xgboost on the segment
 @app.route('/xgblocal',methods=['POST'])
 def XGBlocal():
 	J = request.form.get("J")
@@ -86,8 +120,8 @@ def XGBlocal():
 		b=Getlabel(request.form.get("ids"))
 	else:
 		b =Getlabel(session['pf'])
-	return Myfun_xgb1(b,J)+J
-	
+	return Myfun_xgb(b,J,session['XGB_SHAP_file'],session['original_file'],session['pb'])+J
+#	return a string contains the feature importances, and an index J(will be used in frontend)	
 
 
 if __name__ =="__main__":
